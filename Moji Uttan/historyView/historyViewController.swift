60d67bb4b7c40ff8d7ae611df00045a965d2ad91
Naoya Takahashi <41532603+NaoyTakahashi@users.noreply.github.com>
2019-02-02 01:26:51 +0900

    起動はできるようになった いまだにRealmの呼び出し方がよくわかってない

--- Moji Uttan/historyView/historyViewController.swift
+++ Moji Uttan/historyView/historyViewController.swift
@@ -9,13 +9,45 @@
 import UIKit
 import RealmSwift
 
-class historyViewController: UIViewController {
+class historyViewController: UITableViewController  {
+    
+    var hitorydate: Results<history>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 永続化されているデータを取りだす
+        do{
+            let realm = try Realm()
+            hitorydate = realm.objects(history.self)
+            tableView.reloadData()
+        }catch{
+            
+        }
+        
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int {
+        return hitorydate.count
+    }
+    
+    internal override func tableView(_ tableView: UITableView, cellForRowAt indexpath: IndexPath)->UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        let object = hitorydate[indexpath.row]
+        
+        cell.textLabel?.text = object.title
+        return cell
+    }
     
 
 }
