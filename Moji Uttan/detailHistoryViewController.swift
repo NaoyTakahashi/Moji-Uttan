6b8784db443adc54627d1742de0c23d3aa862725
Naoya Takahashi <41532603+NaoyTakahashi@users.noreply.github.com>
2019-02-11 16:11:50 +0900

    履歴から詳細をお見れるように画面を追加 形は同じだからOutputButtonからの遷移でも使えるかやってみる

---
+++ Moji Uttan/detailHistoryViewController.swift
@@ -0,0 +1,27 @@
+//
+//  detailHistoryView.swift
+//  Moji Uttan
+//
+//  Created by 高橋直也 on 2019/02/09.
+//  Copyright © 2019 高橋直也. All rights reserved.
+//
+
+import UIKit
+
+class detailHistoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBOutlet weak var titleDetailView: UITextView!
+    @IBOutlet weak var textDetailView: UITextView!
+    
+    
+   
+    
+
+}
